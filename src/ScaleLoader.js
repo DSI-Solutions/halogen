1cf8301ff12ab558dbdce8729cf82aa9b94a7a21
Michael Scragg <michael.scragg@dsisolutions.biz>
2017-09-27 10:08:45 -0500

    refactor(ScaleLoader): ES6 + React16 compatability.

--- src/ScaleLoader.js
+++ src/ScaleLoader.js
@@ -1,119 +1,96 @@
-var React = require('react');
-var assign = require('domkit/appendVendorPrefix');
-var insertKeyframesRule = require('domkit/insertKeyframesRule');
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import appendVendorPrefix from 'domkit/appendVendorPrefix';
+import insertKeyframesRule from 'domkit/insertKeyframesRule';
 
-/**
- * @type {Object}
- */
-var keyframes = {
-    '0%': {
-        transform: 'scaley(1.0)'
-    },
-    '50%': {
-        transform: 'scaley(0.4)'
-    },
-    '100%': {
-        transform: 'scaley(1.0)'
+class ScaleLoader extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.keyframes = {
+      '0%': {
+        transform: 'scaley(1.0)',
+      },
+      '50%': {
+        transform: 'scaley(0.4)',
+      },
+      '100%': {
+        transform: 'scaley(1.0)',
+      },
+    };
+
+    this.animationName = insertKeyframesRule(this.keyframes);
+  }
+
+  getLineStyle() {
+    return {
+      backgroundColor: this.props.color,
+      borderRadius: this.props.radius,
+      height: this.props.height,
+      margin: this.props.margin,
+      verticalAlign: this.props.verticalAlign,
+      width: this.props.width,
+    };
+  }
+
+  getAnimationStyle(i) {
+    const animation = [this.animationName, '1s', `${i * 0.1}s`, 'infinite', 'cubic-bezier(.2,.68,.18,1.08)'].join(' ');
+    const animationFillMode = 'both';
+
+    return {
+      animation,
+      animationFillMode,
+    };
+  }
+
+  getStyle(i) {
+    return appendVendorPrefix(
+      this.getLineStyle(i),
+      this.getAnimationStyle(i),
+      {
+        display: 'inline-block',
+      },
+    );
+  }
+
+  render() {
+    const { loading } = this.props;
+
+    if (loading) {
+      return (
+        <div id={this.props.id} className={this.props.className}>
+          <div style={this.getStyle(1)} />
+          <div style={this.getStyle(2)} />
+          <div style={this.getStyle(3)} />
+          <div style={this.getStyle(4)} />
+          <div style={this.getStyle(5)} />
+        </div>
+      );
     }
+
+    return null;
+  }
+}
+
+ScaleLoader.defaultProps = {
+  color: '#ffffff',
+  height: '35px',
+  loading: true,
+  margin: '2px',
+  radius: '2px',
+  width: '4px',
 };
 
-/**
- * @type {String}
- */
-var animationName = insertKeyframesRule(keyframes);
+ScaleLoader.propTypes = {
+  className: PropTypes.string,
+  color: PropTypes.string,
+  height: PropTypes.string,
+  id: PropTypes.string,
+  loading: PropTypes.bool,
+  margin: PropTypes.string,
+  radius: PropTypes.string,
+  verticalAlign: PropTypes.string,
+  width: PropTypes.string,
+};
 
-var Loader = React.createClass({
-    /**
-     * @type {Object}
-     */
-    propTypes: {
-        loading: React.PropTypes.bool,
-        color: React.PropTypes.string,
-        height: React.PropTypes.string,
-        width: React.PropTypes.string,
-        margin: React.PropTypes.string,
-        radius: React.PropTypes.string
-    },
-
-    /**
-     * @return {Object}
-     */
-    getDefaultProps: function() {
-        return {
-            loading: true,
-            color: '#ffffff',
-            height: '35px',
-            width: '4px',
-            margin: '2px',
-            radius: '2px'
-        };
-    },
-
-    /**
-     * @return {Object}
-     */
-    getLineStyle: function() {
-        return {
-            backgroundColor: this.props.color,
-            height: this.props.height,
-            width: this.props.width,
-            margin: this.props.margin,
-            borderRadius: this.props.radius,
-            verticalAlign: this.props.verticalAlign
-        };
-    },
-
-    /**
-     * @param  {Number} i
-     * @return {Object}
-     */
-    getAnimationStyle: function(i) {
-        var animation = [animationName, '1s', (i * 0.1) + 's', 'infinite', 'cubic-bezier(.2,.68,.18,1.08)'].join(' ');
-        var animationFillMode = 'both';
-
-        return {
-            animation: animation,
-            animationFillMode: animationFillMode
-        };
-    },
-
-    /**
-     * @param  {Number} i
-     * @return {Object}
-     */
-    getStyle: function(i) {
-        return assign(
-            this.getLineStyle(i),
-            this.getAnimationStyle(i),
-            {
-                display: 'inline-block'
-            }
-        );
-    },
-
-    /**
-     * @param  {Boolean} loading
-     * @return {ReactComponent || null}
-     */
-    renderLoader: function(loading) {
-        if (loading) {
-            return (
-                <div id={this.props.id} className={this.props.className}>
-                    <div style={this.getStyle(1)}></div>
-                    <div style={this.getStyle(2)}></div>
-                    <div style={this.getStyle(3)}></div>
-                    <div style={this.getStyle(4)}></div>
-                    <div style={this.getStyle(5)}></div>
-                </div>
-            );
-        }
-
-        return null;
-    },
-
-    render: function() {
-        return this.renderLoader(this.props.loading);
-    }
-});
-
-module.exports = Loader;
+export default ScaleLoader;
